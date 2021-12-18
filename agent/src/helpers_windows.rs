96fc8f5cef5da58c4db8072fd4e52bf9c3539279
Jan Harasym <jharasym@linux.com>
2021-12-18 17:22:02 +0100

    initial windows agent testing with winapi

--- agent/src/helpers_windows.rs
+++ agent/src/helpers_windows.rs
@@ -1,18 +1,65 @@
+use std::ffi::OsStr;
+use std::os::windows::ffi::OsStrExt;
+use std::iter::once;
+use std::os::windows::prelude::*;
+
 use crate::winapi::um::lmaccess;
+use crate::winapi::um::winnt::{BOOLEAN, LONG, LPCWSTR, LPWSTR, PSID, PVOID, PZPWSTR, SID_NAME_USE};
+use crate::winapi::shared::minwindef::{BOOL, BYTE, DWORD, FILETIME, LPBYTE, LPDWORD, LPVOID, PBYTE, ULONG};
+//use winapi::shared::lmcons::NET_API_STATUS;
 
-const ADMIN_GROUP: String = "Administrators".to_string();
+fn to_wchar(string: &str) -> Vec<u16> {
+    OsStr::new(string).encode_wide(). chain(Some(0).into_iter()).collect()
+}
 
-pub fn add_user() -> Result<bool, Err> {
+fn to_dword(word: u32) -> u32 {
+    word
+}
+
+const ADMIN_GROUP: &str = "Administrators";
+
+pub fn add_user(user: String, password: String) -> Result<bool, ()> {
+    println!("Hello from windows {}", user.to_string());
+    // Info: https://docs.microsoft.com/en-us/windows/win32/api/lmaccess/ns-lmaccess-user_info_1
+    let buf = lmaccess::USER_INFO_1 {
+        usri1_name: to_wchar(&user).as_mut_ptr(),
+        usri1_password: to_wchar(&password).as_mut_ptr(),
+        usri1_password_age: to_dword(0),
+        usri1_priv: to_dword(0),
+        usri1_home_dir: to_wchar(&format!("C:\\users\\{}", &user)).as_mut_ptr(),
+        usri1_comment: to_wchar(&"Added By Statham").as_mut_ptr(),
+        usri1_flags: to_dword(0),
+        usri1_script_path: to_wchar("").as_mut_ptr(),
+    };
+    let res = lmaccess::NetUserAdd(
+        to_wchar("").as_mut_ptr(),
+        1,
+        &mut buf,
+        to_wchar("").as_mut_ptr(),
+    );
+    println!("{:#}", res);
+    Ok(true)
+}
+
+pub fn add_to_admin_group(user: String) -> Result<bool, ()> {
     unimplemented!()
 }
 
-pub fn add_to_admin_group(&user: String) -> Result<bool, Err> {
-    unimplemented!()
-}
-
-pub fn check_user(&user: String) -> Result<bool, Err> {
+pub fn check_user(user: String) -> Result<bool, ()> {
     /*
         Check if a user exists and is in the Admin group
     */
     unimplemented!()
+}
+
+#[test]
+fn test_user_in_admin_group_yes() {
+    fn does_user_exist(user: String) -> bool {
+        true
+    };
+    let user = "jmh".to_string();
+    let password = "jmh".to_string();
+    assert_eq!(add_user(user.to_owned(), password.to_owned()).unwrap(), true);
+    assert_eq!(does_user_exist(user.to_owned()), true);
+
 }
--- agent/src/main.rs
+++ agent/src/main.rs
@@ -1,12 +1,33 @@
+use std::io::Error;
+
 #[cfg(target_os = "linux")]
 #[path = "helpers_linux.rs"]
 mod helpers;
 
 #[cfg(target_os = "windows")]
+extern crate winapi;
+
+#[cfg(target_os = "windows")]
 #[path = "helpers_windows.rs"]
 mod helpers;
 
+#[cfg(target_os = "windows")]
+fn print_message(msg: &str) -> Result<i32, Error> {
+    use std::ffi::OsStr;
+    use std::iter::once;
+    use std::os::windows::ffi::OsStrExt;
+    use std::ptr::null_mut;
+    use winapi::um::winuser::{MB_OK, MessageBoxW};
+    let wide: Vec<u16> = OsStr::new(msg).encode_wide().chain(once(0)).collect();
+    let ret = unsafe {
+        MessageBoxW(null_mut(), wide.as_ptr(), wide.as_ptr(), MB_OK)
+    };
+    if ret == 0 { Err(Error::last_os_error()) }
+    else { Ok(ret) }
+}
+
 fn main() {
-    helpers::add_user().unwrap();
+    print_message("Hello World!!!").unwrap();
+    helpers::add_user("Admin".to_string()).unwrap();
     println!("Hello, world!");
 }
